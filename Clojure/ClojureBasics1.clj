11d4426a30530f664723d0b475561cc23add8d51
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2022-11-28 13:20:14 -0500

    Added namespace example

--- Clojure/ClojureBasics1.clj
+++ Clojure/ClojureBasics1.clj
@@ -59,23 +59,6 @@
  (is (take 2 a) (seq [10 20])) 
  (is (= (interleave a (sort b)) (seq '(10 5 20 15 30 25))))) 
  
- ; Name spaces
-(ns library)        ; Going to work in this ns
-
-(def x 5)           ; Map symbol x to a var
-(defn average [x y] ; Map symbol average to a var
-   (/ (+ x y) 2))   ; Local x and y are not vars
-
-(println "AVERAGE of 2 and 4 %n" (average 2 4))
-
-(def x 8)           ; Vars are mutable
-
-(ns application)    ; Switch to new namespace
-
-(println "Library x is %n " library/x)
-(println "AVERAGE of 2 and 4 %n" (library/average 2 4))
-(use 'library)
-(println "Library x is %n " x)
 
 ; More on vars
  (use 'clojure.test) 
